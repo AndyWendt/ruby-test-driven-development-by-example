3df8adee44eb47ea8960c2b754ba7fd6770d4474
Andy Wendt <andy@awendt.com>
2019-08-10 21:14:38 -0600

    Consolidate tests to prep for money test

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -15,22 +15,15 @@ RSpec.describe 'Money' do
       five = ::Money::Dollar.new(5)
       expect(::Money::Dollar.new(10)).to eq(five.times(2))
       expect(::Money::Dollar.new(15)).to eq(five.times(3))
-    end
 
-    it 'compares equality' do
-      expect(::Money::Dollar.new(5) == ::Money::Dollar.new(5)).to eq(true)
-      expect(::Money::Dollar.new(5) == ::Money::Dollar.new(6)).to eq(false)
-    end
-  end
-
-  context 'Franc' do
-    it 'multiplies' do
       five = ::Money::Franc.new(5)
       expect(::Money::Franc.new(10)).to eq(five.times(2))
       expect(::Money::Franc.new(15)).to eq(five.times(3))
     end
 
     it 'compares equality' do
+      expect(::Money::Dollar.new(5) == ::Money::Dollar.new(5)).to eq(true)
+      expect(::Money::Dollar.new(5) == ::Money::Dollar.new(6)).to eq(false)
       expect(::Money::Franc.new(5) == ::Money::Franc.new(5)).to eq(true)
       expect(::Money::Franc.new(5) == ::Money::Franc.new(6)).to eq(false)
     end
